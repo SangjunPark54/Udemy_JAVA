f9e11f38366b3e296ffe17bd78fe8df713e9d7fb
seanpark54 <seanpark@sk.com>
2023-08-14 16:26:02 +0900

    test

---
+++ src/udemyjava/multiple_tables.java
@@ -0,0 +1,33 @@
+package udemyjava;
+
+public class multiple_tables {
+    public static void tables(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+//            System.out.println("5 * 3");
+//            System.out.println(15);
+//            System.out.println("______________");
+//            int number = 5;
+//
+//            int j;
+//            int h;
+//            for(j = 1; j < 10; ++j) {
+//                h = number * j;
+//                System.out.println("" + number + " * " + j + " = " + h);
+//            }
+//
+//            System.out.println("______________");
+//
+//            for(j = 1; j < 10; ++j) {
+//                System.out.println("------ " + j + "구구단 -----");
+//
+//                for(h = 1; h < 10; ++h) {
+//                    int var10000 = j * h;
+//                    System.out.printf("%d * %d = %d", j, h, j * h).println();
+//                }
+//            }
+//
+//            System.out.printf("%d + %d + %d = %d", 5, 6, 7, 18).println();
+//        }
+//    }
