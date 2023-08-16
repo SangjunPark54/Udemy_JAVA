4cb353a99f19ecd207acc5abbdd240f4fcc1203a
seanpark54 <seanpark@sk.com>
2023-08-16 16:09:07 +0900

    dev

--- 7. IDP/Java/udemy/udemy/src/udemy/_01_multiple_tables.java
+++ 7. IDP/Java/udemy/udemy/src/udemy/_01_multiple_tables.java
@@ -5,10 +5,17 @@ public class _01_multiple_tables {
         int i;
         int j;
         for (i=0; i< 10; i++) {
-            System.out.println(i + " 구구단");
+            System.out.println(i + " 단 구구단");
             for (j=0; j < 10; j++) {
                 System.out.printf("%d * %d = %d", i, j, i * j).println();
             }
         }
+        System.out.println(Math.min(5, 10));
+
     }
 }
+/*
+ \n : 줄바꿈
+ \ : Escape 특수문자를 String으로 인식
+ \t : Tap(탭) 출력
+ */
