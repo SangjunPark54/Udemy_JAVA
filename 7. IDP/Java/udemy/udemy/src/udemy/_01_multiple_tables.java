fe9e6801b5588373693be1267ef408742a2cf9d1
seanpark54 <seanpark@sk.com>
2023-08-16 15:24:42 +0900

    dev

---
+++ 7. IDP/Java/udemy/udemy/src/udemy/_01_multiple_tables.java
@@ -0,0 +1,14 @@
+package udemy;
+
+public class _01_multiple_tables {
+    public static void main (String[] args) {
+        int i;
+        int j;
+        for (i=0; i< 10; i++) {
+            System.out.println(i + " 구구단");
+            for (j=0; j < 10; j++) {
+                System.out.printf("%d * %d = %d", i, j, i * j).println();
+            }
+        }
+    }
+}
