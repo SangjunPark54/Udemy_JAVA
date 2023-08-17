4ebcc35947051a20a7544487137bd4efe8b1c1cd
SangjunPark <seanpark@sk.com>
2023-08-17 17:17:38 +0900

    update

--- 7. IDP/Java/udemy/udemy/src/udemy/_01_multiple_tables.java
+++ 7. IDP/Java/udemy/udemy/src/udemy/_01_multiple_tables.java
@@ -3,8 +3,8 @@ public class _01_multiple_tables {
     public static void main (String[] args) {
         int i;
         int j;
-        for (i=0; i< 10; i++) {
-            System.out.println(i + " 구구단");
+        for (i=0; i < 10; i++) {
+            System.out.println(i + " 단 구구단");
             for (j=0; j < 10; j++) {
                 System.out.printf("%d * %d = %d", i, j, i * j).println();
             }
---
+++ 7. IDP/Java/udemy/udemy/src/udemy/_03_if_condition.java
@@ -0,0 +1,18 @@
+package udemy;
+
+public class _03_if_condition {
+    public static void main (String[] args) {
+        int i = 6;
+        if (i == 5) {
+            System.out.println("i is odd"); //true
+        }
+        System.out.println("i is prime"); //true // false
+
+        int k = 6;
+        if (k == 5) {
+            System.out.println("i is odd"); //true
+            System.out.println("i is prime"); //true
+        }
+
+    }
+}
