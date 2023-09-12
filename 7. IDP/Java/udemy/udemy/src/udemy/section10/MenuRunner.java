2cb6bce377ccffefac15e59191bbc234d1870c7e
seanpark54 <seanpark@sk.com>
2023-09-12 17:30:04 +0900

    sec10

---
+++ 7. IDP/Java/udemy/udemy/src/udemy/section10/MenuRunner.java
@@ -0,0 +1,49 @@
+package udemy.section10;
+
+import java.util.Scanner;
+
+public class MenuRunner {
+    public static void main(String[] args) {
+        // Type Obj = new Type(argument);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Number1: ");
+        int number1 = scanner.nextInt();
+        System.out.println("The number you Enter is - " + number1);
+
+        System.out.print("Enter Number2: ");
+        int number2 = scanner.nextInt();
+        System.out.println("The number you Enter is - " + number2);
+
+        System.out.println("Choose Operations: ");
+        System.out.println("1: add");
+        System.out.println("2: negative");
+        System.out.println("3: divide");
+        System.out.println("4: multiply");
+
+        System.out.println("Enter Choice: ");
+        int choice = scanner.nextInt();
+
+        System.out.println("the operation you chooses are ");
+        System.out.println("number1 " + number1);
+        System.out.println("number2 " + number2);
+        System.out.println("choice " + choice);
+
+        if(choice == 1) {
+            int result = number1 + number2;
+            System.out.println("Result is " + result);
+        } else if(choice == 2) {
+            int result = number1 - number2;
+            System.out.println("Result is " + result);
+        } else if(choice ==3) {
+            int result = number1 / number2;
+            System.out.println("Result is " + result);
+        } else if (choice == 4) {
+            int result = number1 * number2;
+            System.out.println("Result is " + result);
+        } else {
+            System.out.println("Invalid Operation");
+        }
+
+    }
+}
---
+++ 7. IDP/Java/udemy/udemy/src/udemy/section10/puzzle.java
@@ -0,0 +1,50 @@
+package udemy.section10;
+
+public class puzzle {
+    public static void main(String[] args) {
+        puzzle2();
+    }
+
+    private static void puzzle1() {
+        int k = 15;
+        if (k > 20) {
+            System.out.println(1);
+        } else if (k > 10) {
+            System.out.println(2);
+        } else if (k < 20) {
+            System.out.println(3);
+        } else {
+            System.out.println(4);
+        }
+    }
+// 조건이 참이 되면 뒤의 조건은 실행되지 않는다.
+    private static void puzzle2() {
+        int l = 15;
+        if (l < 20) {
+            System.out.println("l<20"); //
+        }
+        if (l > 20) {
+            System.out.println("l>20");
+        } else System.out.println("Who am I?"); //
+        // 위 2개의 if는 서로 독립적인 문이기 때문에 2개의 로직이 실행됨.
+        // 위 2개의 if는 서로 연결되있지 않음.
+        // 블럭을 사용하여 구분 지어주는게 좋음.
+    }
+    private static void puzzle3 () {
+        int m = 15;
+
+        if (m > 20) {
+            if (m < 20) {
+                System.out.println("m>20");
+            } else {
+                System.out.println("Who am I?");
+            }
+        }
+    }
+    private static void puzzle4() {
+        int x1 = 0;
+        //Condition in if should always be boolean
+        //if(x1) {} // COMPILER ERROR
+        //if(x1=0) {} / COMPILER ERROR/ == instead of = // = assignment // == is 비교연산자
+    }
+}
